064039dbe5b3c378882ad04f5eebd9aade5e9f6a
hmhieu18 <hmhieu18@apcs.vn>
2022-07-28 21:39:49 +0700

    fix

--- Task1-ActionSpotting/TemporallyAwarePooling/src/train.py
+++ Task1-ActionSpotting/TemporallyAwarePooling/src/train.py
@@ -184,8 +184,8 @@ def test(dataloader, model, model_name):
                 desc += f'(it:{data_time.val:.3f}s) '
                 t.set_description(desc)
 
-    print("all_labels: ", all_labels.shape)
-    print("all_outputs: ", all_outputs.shape)
+    print("all_labels: ", len(all_labels))
+    print("all_outputs: ", len(all_outputs))
     AP = []
     for i in range(1, dataloader.dataset.num_classes+1):
         AP.append(average_precision_score(np.concatenate(all_labels)
