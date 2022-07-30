46ec48fc0c277d6dfb7a057d82584f4bfe0e047e
hmhieu18 <hmhieu18@apcs.vn>
2022-07-31 00:59:17 +0700

    add file

---
+++ Task1-ActionSpotting/TemporallyAwarePooling_read_data_from_file/src/dataset.py
@@ -0,0 +1,234 @@
+from torch.utils.data import Dataset
+
+import numpy as np
+import random
+import os
+import time
+
+
+from tqdm import tqdm
+
+import torch
+
+import logging
+import json
+
+from SoccerNet.Downloader import getListGames
+from SoccerNet.Downloader import SoccerNetDownloader
+from SoccerNet.Evaluation.utils import AverageMeter, EVENT_DICTIONARY_V2, INVERSE_EVENT_DICTIONARY_V2
+from SoccerNet.Evaluation.utils import EVENT_DICTIONARY_V1, INVERSE_EVENT_DICTIONARY_V1
+
+
+def getShapeWithoutLoading(numpyFile):
+    with open(numpyFile, 'rb') as f:
+        major, minor = np.lib.format.read_magic(f)
+        shape, fortran, dtype = np.lib.format.read_array_header_1_0(f)
+    return shape
+
+
+def feats2clip(feats, stride, clip_length, padding="replicate_last", off=0):
+    if padding == "zeropad":
+        print("beforepadding", feats.shape)
+        pad = feats.shape[0] - int(feats.shape[0]/stride)*stride
+        print("pad need to be", clip_length-pad)
+        m = torch.nn.ZeroPad2d((0, 0, clip_length-pad, 0))
+        feats = m(feats)
+        print("afterpadding", feats.shape)
+        # nn.ZeroPad2d(2)
+
+    idx = torch.arange(start=0, end=feats.shape[0]-1, step=stride)
+    idxs = []
+    for i in torch.arange(-off, clip_length-off):
+        idxs.append(idx+i)
+    idx = torch.stack(idxs, dim=1)
+
+    if padding == "replicate_last":
+        idx = idx.clamp(0, feats.shape[0]-1)
+    # print(idx)
+    return feats[idx, ...]
+# zero pad to the end of the clip
+
+
+def padding(feats, shape):
+    if(feats.shape < shape):
+        result = np.zeros(shape)
+        result[:feats.shape[0],
+               :feats.shape[1]] = feats
+    else:
+        result = feats[0:shape[0], :]
+    return result
+
+
+class SoccerNetClips(Dataset):
+    def __init__(self, path, features="ResNET_PCA512.npy", split=["train"], version=1,
+                 framerate=2, window_size=15, listGames=None,):
+        self.path = path
+        self.listGames = listGames
+        self.features = features
+        self.window_size_frame = window_size*framerate
+        self.version = version
+        if version == 1:
+            self.num_classes = 3
+            self.labels = "Labels.json"
+        elif version == 2:
+            self.dict_event = EVENT_DICTIONARY_V2
+            self.num_classes = 17
+            self.labels = "Labels-v2.json"
+
+        logging.info("Checking/Download features and labels locally")
+        downloader = SoccerNetDownloader(path)
+        downloader.downloadGames(files=[
+                                 self.labels, f"1_{self.features}", f"2_{self.features}"], split=split, verbose=False, randomized=True)
+
+        logging.info("Pre-compute clips")
+
+        self.game_feats_file = list()
+        self.game_labels = list()
+
+        # game_counter = 0
+        for game in tqdm(self.listGames):
+            #get filename
+            feats1_filename = os.path.join(
+                self.path, game, f"1_{self.features}")
+            feats2_filename = os.path.join(
+                self.path, game, f"2_{self.features}")
+        
+            self.game_feats_file.append(feats1_filename)
+            self.game_feats_file.append(feats2_filename)
+
+            feats1_shape = getShapeWithoutLoading(feats1_filename)
+            feats2_shape = getShapeWithoutLoading(feats2_filename)
+
+            labels = json.load(open(os.path.join(self.path, game, self.labels)))
+
+            label_half1 = np.zeros((feats1_shape[0], self.num_classes+1))
+            label_half1[:,0]=1 # those are BG classes
+            label_half2 = np.zeros((feats1_shape[0], self.num_classes+1))
+            label_half2[:,0]=1 # those are BG classes
+
+
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            clip_feat (np.array): clip of features.
+            clip_labels (np.array): clip of labels for the segmentation.
+            clip_targets (np.array): clip of targets for the spotting.
+        """
+        # print("index", index)
+        # print("game_feats", self.game_feats[index].shape)
+        return self.game_feats[index], self.game_labels[index]
+        # return self.game_feats[index, :, :], self.game_labels[index, :]
+
+    def __len__(self):
+        return len(self.game_feats)
+
+
+class SoccerNetClipsTesting(Dataset):
+    def __init__(self, path, features="ResNET_PCA512.npy", split=["test"], version=1,
+                 framerate=2, window_size=15, listGames=None,):
+        self.path = path
+        self.listGames = listGames
+        self.features = features
+        self.window_size_frame = window_size*framerate
+        self.framerate = framerate
+        self.version = version
+        self.split = split
+        if version == 1:
+            self.dict_event = EVENT_DICTIONARY_V1
+            self.num_classes = 3
+            self.labels = "Labels.json"
+        elif version == 2:
+            self.dict_event = EVENT_DICTIONARY_V2
+            self.num_classes = 17
+            self.labels = "Labels-v2.json"
+
+        logging.info("Checking/Download features and labels locally")
+        downloader = SoccerNetDownloader(path)
+        for s in split:
+            if s == "challenge":
+                downloader.downloadGames(files=[f"1_{self.features}", f"2_{self.features}"], split=[
+                                         s], verbose=False, randomized=True)
+            else:
+                downloader.downloadGames(files=[self.labels, f"1_{self.features}", f"2_{self.features}"], split=[
+                                         s], verbose=False, randomized=True)
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            feat_half1 (np.array): features for the 1st half.
+            feat_half2 (np.array): features for the 2nd half.
+            label_half1 (np.array): labels (one-hot) for the 1st half.
+            label_half2 (np.array): labels (one-hot) for the 2nd half.
+        """
+        # Load features
+        feat_half1 = np.load(os.path.join(
+            self.path, self.listGames[index], "1_" + self.features))
+        feat_half1 = feat_half1.reshape(-1, feat_half1.shape[-1])
+        feat_half2 = np.load(os.path.join(
+            self.path, self.listGames[index], "2_" + self.features))
+        feat_half2 = feat_half2.reshape(-1, feat_half2.shape[-1])
+
+        # Load labels
+        label_half1 = np.zeros((feat_half1.shape[0], self.num_classes))
+        label_half2 = np.zeros((feat_half2.shape[0], self.num_classes))
+
+        # check if annoation exists
+        if os.path.exists(os.path.join(self.path, self.listGames[index], self.labels)):
+            labels = json.load(
+                open(os.path.join(self.path, self.listGames[index], self.labels)))
+
+            for annotation in labels["annotations"]:
+
+                time = annotation["gameTime"]
+                event = annotation["label"]
+
+                half = int(time[0])
+
+                minutes = int(time[-5:-3])
+                seconds = int(time[-2::])
+                frame = self.framerate * (seconds + 60 * minutes)
+
+                if self.version == 1:
+                    if "card" in event:
+                        label = 0
+                    elif "subs" in event:
+                        label = 1
+                    elif "soccer" in event:
+                        label = 2
+                    else:
+                        continue
+                elif self.version == 2:
+                    if event not in self.dict_event:
+                        continue
+                    label = self.dict_event[event]
+
+                value = 1
+                if "visibility" in annotation.keys():
+                    if annotation["visibility"] == "not shown":
+                        value = -1
+
+                if half == 1:
+                    frame = min(frame, feat_half1.shape[0]-1)
+                    label_half1[frame][label] = value
+
+                if half == 2:
+                    frame = min(frame, feat_half2.shape[0]-1)
+                    label_half2[frame][label] = value
+
+        feat_half1 = feats2clip(torch.from_numpy(feat_half1),
+                                stride=1, off=int(self.window_size_frame/2),
+                                clip_length=self.window_size_frame)
+
+        feat_half2 = feats2clip(torch.from_numpy(feat_half2),
+                                stride=1, off=int(self.window_size_frame/2),
+                                clip_length=self.window_size_frame)
+
+        return self.listGames[index], feat_half1, feat_half2, label_half1, label_half2
+
+    def __len__(self):
+        return len(self.listGames)
---
+++ Task1-ActionSpotting/padding_features/src/main.py
@@ -0,0 +1,164 @@
+import os
+import logging
+from datetime import datetime
+import time
+import numpy as np
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+import torch
+
+from dataset import SoccerNetClips, SoccerNetClipsTesting  # ,SoccerNetClipsOld
+from model import Model
+from train import trainer, test, testSpotting
+from loss import NLLLoss
+from SoccerNet.Downloader import getListGames
+
+
+def main(args):
+
+    logging.info("Parameters:")
+    for arg in vars(args):
+        logging.info(arg.rjust(15) + " : " + str(getattr(args, arg)))
+    # load list games from npy
+    if(args.train_list is ""):
+        train_list = np.array(getListGames(['train']))
+    else:
+        train_list = np.load(args.train_list)
+
+    if(args.test_list is ""):
+        test_list = np.array(getListGames(['test']))
+    else:
+        test_list = np.load(args.test_list)
+
+    if(args.valid_list is ""):
+        val_list = np.array(getListGames(['valid']))
+    else:
+        val_list = np.load(args.valid_list)
+
+    percent = args.percent
+    # random select 100 games from train_list
+    train_list_length = len(train_list)
+    train_list = train_list[np.random.choice(
+        train_list.shape[0], int(train_list_length*percent), replace=False)]
+    test_list_length = len(test_list)
+    test_list = test_list[np.random.choice(
+        test_list.shape[0], int(test_list_length*percent), replace=False)]
+    val_list_length = len(val_list)
+    val_list = val_list[np.random.choice(
+        val_list.shape[0], int(val_list_length*percent), replace=False)]
+
+    # create dataset
+    if not args.test_only:
+        dataset_Train = SoccerNetClips(path=args.SoccerNet_path, features=args.features, split=args.split_train,
+                                       version=args.version, framerate=args.framerate, window_size=args.window_size, listGames=train_list)
+        dataset_Valid = SoccerNetClips(path=args.SoccerNet_path, features=args.features, split=args.split_valid,
+                                       version=args.version, framerate=args.framerate, window_size=args.window_size, listGames=val_list)
+        dataset_Test = SoccerNetClips(path=args.SoccerNet_path, features=args.features, split=args.split_test,
+                                        version=args.version, framerate=args.framerate, window_size=args.window_size, listGames=test_list)
+                                        
+    dataset_Test = SoccerNetClipsTesting(path=args.SoccerNet_path, features=args.features, split=args.split_test,
+                                         version=args.version, framerate=args.framerate, window_size=args.window_size, listGames=test_list)
+    return
+
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser(description='context aware loss function',
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--SoccerNet_path',   required=False, type=str,
+                        default="/path/to/SoccerNet/",     help='Path for SoccerNet')
+    parser.add_argument('--features',   required=False, type=str,
+                        default="ResNET_TF2.npy",     help='Video features')
+    parser.add_argument('--max_epochs',   required=False, type=int,
+                        default=1000,     help='Maximum number of epochs')
+    parser.add_argument('--load_weights',   required=False,
+                        type=str,   default=None,     help='weights to load')
+    parser.add_argument('--model_name',   required=False, type=str,
+                        default="NetVLAD++",     help='named of the model to save')
+    parser.add_argument('--test_only',   required=False,
+                        action='store_true',  help='Perform testing only')
+
+    parser.add_argument('--split_train', nargs='+',
+                        default=["train"], help='list of split for training')
+    parser.add_argument('--split_valid', nargs='+',
+                        default=["valid"], help='list of split for validation')
+    parser.add_argument('--split_test', nargs='+',
+                        default=["test", "challenge"], help='list of split for testing')
+
+    parser.add_argument('--version', required=False, type=int,
+                        default=2,     help='Version of the dataset')
+    parser.add_argument('--feature_dim', required=False, type=int,
+                        default=None,     help='Number of input features')
+    parser.add_argument('--evaluation_frequency', required=False,
+                        type=int,   default=10,     help='Number of chunks per epoch')
+    parser.add_argument('--framerate', required=False, type=int,
+                        default=2,     help='Framerate of the input features')
+    parser.add_argument('--window_size', required=False, type=int,
+                        default=15,     help='Size of the chunk (in seconds)')
+    parser.add_argument('--pool',       required=False,
+                        type=str,   default="NetVLAD++", help='How to pool')
+    parser.add_argument('--vocab_size',       required=False, type=int,
+                        default=64, help='Size of the vocabulary for NetVLAD')
+    parser.add_argument('--NMS_window',       required=False,
+                        type=int,   default=30, help='NMS window in second')
+    parser.add_argument('--NMS_threshold',       required=False, type=float,
+                        default=0.0, help='NMS threshold for positive results')
+
+    parser.add_argument('--batch_size', required=False,
+                        type=int,   default=256,     help='Batch size')
+    parser.add_argument('--LR',       required=False,
+                        type=float,   default=1e-03, help='Learning Rate')
+    parser.add_argument('--LRe',       required=False,
+                        type=float,   default=1e-06, help='Learning Rate end')
+    parser.add_argument('--patience', required=False, type=int,   default=10,
+                        help='Patience before reducing LR (ReduceLROnPlateau)')
+
+    parser.add_argument('--GPU',        required=False, type=int,
+                        default=-1,     help='ID of the GPU to use')
+    parser.add_argument('--max_num_worker',   required=False,
+                        type=int,   default=4, help='number of worker to load data')
+    parser.add_argument('--seed',   required=False, type=int,
+                        default=0, help='seed for reproducibility')
+
+    # parser.add_argument('--logging_dir',       required=False, type=str,   default="log", help='Where to log' )
+    parser.add_argument('--loglevel',   required=False,
+                        type=str,   default='INFO', help='logging level')
+    parser.add_argument('--train_list', required=False,
+                        type=str,   default="", help='train list location')
+    parser.add_argument('--valid_list', required=False,
+                        type=str,   default="", help='valid list location')
+    parser.add_argument('--test_list', required=False,
+                        type=str,   default="", help='test list location')
+    parser.add_argument('--percent', required=False, type=float,
+                        default=1.0,     help='percent of data to use')
+
+    args = parser.parse_args()
+
+    # for reproducibility
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+
+    numeric_level = getattr(logging, args.loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % args.loglevel)
+
+    os.makedirs(os.path.join("models", args.model_name), exist_ok=True)
+    log_path = os.path.join("models", args.model_name,
+                            datetime.now().strftime('%Y-%m-%d_%H-%M-%S.log'))
+    logging.basicConfig(
+        level=numeric_level,
+        format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+        handlers=[
+            logging.FileHandler(log_path),
+            logging.StreamHandler()
+        ])
+
+    if args.GPU >= 0:
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(args.GPU)
+
+    start = time.time()
+    logging.info('Starting main function')
+    main(args)
+    logging.info(f'Total Execution Time is {time.time()-start} seconds')
