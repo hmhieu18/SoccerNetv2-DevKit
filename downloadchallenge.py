6ee4ccf4ac3669ed1dcdf50489e9f4d3498d1adf
hmhieu18 <hmhieu18@apcs.vn>
2022-08-01 22:40:38 +0700

    fix

--- downloadchallenge.py
+++ downloadchallenge.py
@@ -1,4 +1,5 @@
 from SoccerNet.Downloader import SoccerNetDownloader
 
 downloader = SoccerNetDownloader('/content/soccetnet')
+downloader.password = 's0cc3rn3t'
 downloader.downloadGames(files=[f"1_224p.mkv", f"2_224p.mkv"], split=['challenge'], verbose=False, randomized=True)
