0f8ffc179b6b19bb310ccf8dac51753fe0da8afa
hmhieu18 <hmhieu18@apcs.vn>
2022-08-20 05:45:23 +0700

    fix

--- Task1-ActionSpotting/TemporallyAwarePooling_audio_vgg_new_nms/src/train.py
+++ Task1-ActionSpotting/TemporallyAwarePooling_audio_vgg_new_nms/src/train.py
@@ -304,7 +304,7 @@ def testSpotting(dataloader, model, model_name, overwrite=True, NMS_window=30, N
                         print("Input2: ", Input2.shape)
                         max_value = np.max(detections_tmp)                        
                         max_index = np.argmax(detections_tmp)
-                        # print(max_index, max_value)
+                        print("Max index", max_index)
                         if Input2[max_index,:]:
                             print("CENTERING")
                             max_index2 = np.argmax(Input2[max_index,:])
